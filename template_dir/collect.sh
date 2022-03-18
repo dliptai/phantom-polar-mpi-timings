b6c292e5bbce311d57431711bc569a5541d601cc
David Liptai <david.liptai@monash.edu>
2022-03-18 13:07:24 +1100

    added option to collect mpi balance statistics

--- template_dir/collect.sh
+++ template_dir/collect.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 get_percentage=0
+get_balance=0
+col=1
+ext=
 
 while [[ "$1" == --* ]]; do
   case $1 in
     --percent)
       get_percentage=1;
+      ;;
+
+    --balance)
+      get_balance=1;
       ;;
 
     *)
@@ -22,8 +29,11 @@ fi
 
 if [[ $get_percentage == 1 ]]; then
   ext='_%'
-else
-  ext=
+  col=5
+fi
+if [[ $get_balance == 1 ]]; then
+  ext='_bal'
+  col=4
 fi
 
 set -eu
@@ -39,30 +49,27 @@ function get_data(){
   local ntasks=$((ntasks_per_node*nodes))
 
   local d="run-${nodes}x${ntasks_per_node}x${cpus_pertask}"
-  local f="$d/polar01.log"
 
   local line ttemp timing timer_a timber_b traw
 
   if [[ $timer = *" "* ]]; then
     timer_a=$(echo $timer | cut -d ' ' -f 1)
     timer_b=$(echo $timer | cut -d ' ' -f 2)
-    line=$(sed -n "/─${timer_a}/,/─${timer_b}/p" $f | tail -1)
+    line=$(sed -n "/─${timer_a}/,/─${timer_b}/p" $d/*01.log | tail -1)
   else
-    line=$(sed -n "/─${timer}/p" $f)
+    line=$(sed -n "/─${timer}/p" $d/*01.log)
   fi
 
   line=$(echo $line | grep '%' | grep ':' | tr -s ' ' | cut -d ':' -f 2)
 
   timing="NaN"
-  if [[ $get_percentage == 1 ]]; then
-    traw=$(echo $line | cut -d ' ' -f 4)
-    if [[ ! -z $traw ]]; then
+  traw=$(echo $line | cut -d ' ' -f $col)
+
+  if [[ ! -z $traw ]]; then
+    if [[ $get_percentage == 1 ]] || [[ $get_balance == 1 ]]; then
       timing=$(echo $traw | cut -d '%' -f 1)
-    fi
-  else
+    else
     # Convert minutes to seconds
-    traw=$(echo $line | cut -d ' ' -f 1)
-    if [[ ! -z $traw ]]; then
       if [[ $traw == *min ]]; then
         ttemp=$(echo $traw | cut -d 'm' -f 1)
         timing=$(echo "print ${ttemp}*60" | perl)
